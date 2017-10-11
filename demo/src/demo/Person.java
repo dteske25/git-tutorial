3a971dca53c92c87c1c158e82e98fe4f0d597db1
bklein18 <31358894+bklein18@users.noreply.github.com>
2017-10-10 20:26:18 -0500

    Implemented toString

--- demo/src/demo/Person.java
+++ demo/src/demo/Person.java
@@ -6,19 +6,23 @@ public class Person {
 		this.lastName = "";
 		this.email = "";
 	}
-	
+
 	public Person(String firstName, String lastName, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 	}
-	
+
 	private String firstName;
 	private String lastName;
 	private String email;
-	
-	
+
+
 	//TODO: Add getters
 	//TODO: Add setters
-	//TODO: Implement a toString function in the format of "lastName, firstName (emailAddress)"
+
+
+	public String toString(){
+		return lastName + ", " + firstName + "(" + emailAddress + ")";
+	}
 }
